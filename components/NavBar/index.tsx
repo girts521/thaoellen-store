b9814f2fe9df4b1e1cab7529ed667a7ce5b8803b
Janis Karcevskis <janiskarcevskis219@gmail.com>
2023-10-21 22:19:16 +0200

    test

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -5,7 +5,7 @@ export default function NavBar () {
 
     return (
         <div className={styles.container}>
-            <h1>Thao</h1>
+            <h1>Thaos</h1>
             <Image src={"/Burger.png"} alt='Burger menu' width={25} height={25} />
             <Image src={"/bag.png"} alt='shopping menu' width={25} height={25} />
         </div>
