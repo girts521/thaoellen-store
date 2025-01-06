3145751efe73f1abb4f2e04be3102844859cdcbf
Girts <girts@gkarcevskis.com>
2025-01-06 22:17:22 +0100

    Added fb pixel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,27 @@ import { Analytics } from '@vercel/analytics/next';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '902105128397022');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      <noscript><img height="1" width="1" style={{display: "none"}}
+      src="https://www.facebook.com/tr?id=902105128397022&ev=PageView&noscript=1"
+      /></noscript>
+      </Head>
       <body className="bg-white text-black">
         <Main />
         <NextScript />
